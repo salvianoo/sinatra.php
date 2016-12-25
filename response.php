a047533fe65b98ea2d7c13cd9b9a0db27f818a41
Salviano Ludgerio <salviano.lfg@gmail.com>
2016-12-25 16:14:56 -0200

    Minor

--- response.php
+++ response.php
@@ -8,19 +8,19 @@ class Response
 
     public function __construct($params)
     {
-        $this->callback   = isset($params['callback']) ? $params['callback'] : null; 
+        $this->callback   = isset($params['callback']) ? $params['callback'] : null;
         $this->statusCode = isset($params['statusCode']) ? $params['statusCode'] : 200;
     }
 
-    //IoC 
+    //IoC
     public function write(IFormatter $formatter): string
     {
         header('Content-Type: application/json');
-        
+
         if (! is_null($this->callback)) {
             $closureObject = $this->callback;
             return $formatter->format($closureObject());
-        } 
+        }
         return $formatter->format('Request Not Found');
     }
 }
